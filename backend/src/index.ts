2e1cda0c94e0ea18a06ed02fde1dcd65cd2d7ca9
aadi90392 <adi375461@gmail.com>
2026-07-13 17:58:56 +0530

    fix login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Application } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import routes from './routes';
 
 dotenv.config();
 
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-  res.send(' Intern Tracker API is running!');
+  res.send('Intern Tracker API is running!');
 });
 
+app.use('/api', routes);
 
 console.log('Connecting to MongoDB Atlas...');
 
@@ -24,9 +24,9 @@ mongoose.connect(process.env.MONGO_URI as string)
   .then(() => {
     console.log('MongoDB Atlas Connected Successfully!');
     app.listen(PORT, () => {
-      console.log(` Server running on http://localhost:${PORT}`);
+      console.log(`Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error(' DB Connection Error:', err.message);
+    console.error('DB Connection Error:', err.message);
   });
